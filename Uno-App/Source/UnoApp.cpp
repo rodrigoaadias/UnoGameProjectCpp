10e7270c4713b40411142eba153cc151d9a6e3d7
Rodrigo Dias <rodrigoaadias@hotmail.com>
2023-12-12 11:29:27 -0300

    Improved general architecture file and created a new one for cards to be cleaner

--- Uno-App/Source/UnoApp.cpp
+++ Uno-App/Source/UnoApp.cpp
@@ -1,3 +1,5 @@
+#include <stack>
+
 #include "GameLifecycleController.h"
 #include "Player.h"
 #include "Core/Core.h"
@@ -8,5 +10,6 @@ int main()
 	Player* player1 = new Player("Player One");
 	Player* player2 = new Player("Player Two");
 	new GameLifecycleController();
+
 	Core::RunEngine();
 }
