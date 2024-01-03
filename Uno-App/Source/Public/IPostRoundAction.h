ab507af004601272780ce9fad2624e1f56907f9e
Rodrigo Dias <rodrigoaadias@hotmail.com>
2024-01-03 15:43:55 -0300

    const correctness on IPostRoundAction

--- Uno-App/Source/Public/IPostRoundAction.h
+++ Uno-App/Source/Public/IPostRoundAction.h
@@ -3,7 +3,14 @@
 
 class IPostRoundAction
 {
+    IPostRoundAction() = default;
 public:
+    IPostRoundAction(const IPostRoundAction& other) = delete;
+    IPostRoundAction(const IPostRoundAction&& other) = delete;
+
+    IPostRoundAction& operator = (const IPostRoundAction& other) = delete;
+    IPostRoundAction& operator = (const IPostRoundAction&& other) = delete;
+
     virtual void Execute(Match* currentMatch) = 0;
     virtual ~IPostRoundAction() = default;
 };
